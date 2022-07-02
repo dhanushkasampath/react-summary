5688c6a7dbbb35f5dbac97c5461de637025bf201
dsampath <dsampath@wiley.com>
2022-07-02 08:24:15 +0530

    New Pages created and Basic routing added

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Install react router dom -> npm install --save react-router-dom
+2. wrap the App component between <BrowserRouter>
+3. define URLs and which pages need to loaded using <Route> component 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import {Route, Routes} from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetupsPage';
+import NewMeetupPage from './pages/NewMeetupPage';
+import FavoritesPage from './pages/FavoritesPage';
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div></div>;
+  return (<div>
+    <MainNavigation/>
+    <Routes>
+      <Route path='/' element={<AllMeetupsPage/>} />
+      <Route path='/new-meetup' element={<NewMeetupPage/>} />
+      <Route path='/favorites' element={<FavoritesPage/>} />
+    </Routes>
+  </div>);
 }
 
 export default App;
