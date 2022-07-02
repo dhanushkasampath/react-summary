b8bb86d32983331b57e27bcda7aba037437b815f
dsampath <dsampath@wiley.com>
2022-07-02 13:03:48 +0530

    layout added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import {Route, Routes} from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetupsPage';
-import NewMeetupPage from './pages/NewMeetupPage';
+import NewMeetupPage from './pages/NewMeetupsPage';
 import FavoritesPage from './pages/FavoritesPage';
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return (<div>
-    <MainNavigation/>
-    <Routes>
-      <Route path='/' element={<AllMeetupsPage/>} />
-      <Route path='/new-meetup' element={<NewMeetupPage/>} />
-      <Route path='/favorites' element={<FavoritesPage/>} />
-    </Routes>
-  </div>);
+  return (
+      <Layout>
+        <Routes>
+          <Route path='/' element={<AllMeetupsPage/>} />
+          <Route path='/new-meetup' element={<NewMeetupPage/>} />
+          <Route path='/favorites' element={<FavoritesPage/>} />
+        </Routes>
+      </Layout>
+  );
 }
 
 export default App;
