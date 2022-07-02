6229b7dbe4fc26a540d0e83b6d3fbfd927140c26
dsampath <dsampath@wiley.com>
2022-07-02 16:32:12 +0530

    made a post request to firebase

--- src/pages/NewMeetupsPage.js
+++ src/pages/NewMeetupsPage.js
@@ -1,10 +1,21 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage(){
+    function addMeetupHandler(meetupData){//This is how we propagate the data upwards.
+        fetch('https://react-prep-b4fd7-default-rtdb.firebaseio.com/meetups.json',
+            {
+            method: 'POST',
+            body: JSON.stringify(meetupData),//data should be send as json to this api
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm/>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </section>
     );
 }
