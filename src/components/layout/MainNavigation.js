7510394f2539ed2e4be77a4bff6aa1cbbc8054a4
dsampath <dsampath@wiley.com>
2022-07-03 20:35:31 +0530

    used the created context in different components.

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,10 @@
 import {Link} from "react-router-dom";
 import classes from './MainNavigation.module.css';
+import {useContext} from "react";
+import FavoritesContext from "../../store/favorite-context";
 
 function MainNavigation(){
+    const favoriteCtx = useContext(FavoritesContext);
     return (
         <header className={classes.header}>
             <div className={classes.logo}>Millennium Fitness Center</div>
@@ -14,12 +17,17 @@ function MainNavigation(){
                         <Link to='/new-meetup'>Add New Meetup</Link>
                     </li>
                     <li>
-                        <Link to='/favorites'>My Favorites</Link>
+                        <Link to='/favorites'>
+                            My Favorites
+                            <span className={classes.badge}>{favoriteCtx.totalFavorites}</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
         </header>
     );
 }
+//Link component is similart to anchor tag. but anchor tag makes api call to back end once clicked. but Link does
+// not. as all pages loaded at the initial loading time.
 
 export default MainNavigation;
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,7 +1,27 @@
 import classes from "./MeetupItem.module.css";
 import Card from "../ui/Card";
+import {useContext} from "react";
+import FavoritesContext from "../../store/favorite-context";
 
 function MeetupItem(props) {
+    const favoritesCtx = useContext(FavoritesContext)//this need the created Context using createContext() as argument
+
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+    function toggleFavoriteStatusHandler(){
+        if(itemIsFavorite){
+            //then remove from favorite context
+            favoritesCtx.removeFavorite(props.id);
+        }else{
+            favoritesCtx.addFavorite({
+                id: props.id,
+                title: props.title,
+                description: props.description,
+                image: props.image,
+                address: props.address
+            })
+        }
+    }
+
     return (
         <li className={classes.item}>
             <Card>
@@ -14,7 +34,9 @@ function MeetupItem(props) {
                     <p>{props.description}</p>
                 </div>
                 <div className={classes.actions}>
-                    <button>To Favorites</button>
+                    <button onClick={toggleFavoriteStatusHandler}>
+                        {itemIsFavorite ? 'Remove from favorites' : 'To Favorites'}
+                    </button>
                 </div>
             </Card>
         </li>
